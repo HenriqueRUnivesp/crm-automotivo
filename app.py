ecbc7ff60541052254a6f3c3a2b288e86d4d5fd9
HenriqueRUnivesp <henriquerosa@msn.com>
2026-06-27 14:16:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -264,4 +264,5 @@ def mudar_status(lead_id):
 
 if __name__ == "__main__":
     inicializar_banco(app)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
